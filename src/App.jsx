f59d54ef1085ef58357e9f89c8dacedf21d0f6f0
ismsahin <ismsahin6182@hotmail.com>
2023-11-10 21:46:22 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Nav from './components/Nav';
+import footer from './components/footer'
+
+
+function App() {
+  return (
+    <>
+      <div w-full h-screen>
+        <Nav/>
+        <footer/>
+      </div>
+    </>
+  )
+}
+
+export default App
