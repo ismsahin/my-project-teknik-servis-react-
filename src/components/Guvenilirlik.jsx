272704055f6d5c5b059005c4395a0726105e4d9f
ismsahin <ismsahin6182@hotmail.com>
2023-11-11 17:05:10 +0300

    içerik ekleme

--- src/components/Guvenilirlik.jsx
+++ src/components/Guvenilirlik.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const Guvenilirlik = () => {
   return (
     <div className='   xl:container '>
-            <div className=' w-full mx-auto h-150 object-cover bg-cover bg-center bg-[url("https://images.pexels.com/photos/7682340/pexels-photo-7682340.jpeg")]'>
-                <h1 className='text-center'>merhaba</h1>
+            <div className=' w-full mx-auto h-150 object-cover bg-cover bg-center grid grid-cols-2 bg-[url("https://images.pexels.com/photos/9242852/pexels-photo-9242852.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")] bg-center'>
+                <div className=''></div>
+                <div className=''></div>
             </div>
 
 
--- src/components/servisDestek.jsx
+++ src/components/servisDestek.jsx
@@ -5,9 +5,21 @@ const servisDestek = () => {
     return (
         <div className='   xl:container '>
             <div className='my-6 w-full bg-gradient-to-b mx-auto h-150 object-cover bg-cover bg-center bg-[url("https://images.pexels.com/photos/7682340/pexels-photo-7682340.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")]'>
-                <div className='justify-center'>
-                    <h1 className='text-center '>merhaba</h1>
+                <div className='  grid grid-cols-2 h-150 grid grid-rows-4 '>
+                    <div className=''></div>
+                    <div className=''></div>
+                    <div className=''></div>
+                    <div className='mb-0  text-4xl m-auto text-white font-bold bg-dark_mode rounded-md ms-0 bg-opacity-75'>7/24 Müşteri Hizmetleri </div>
+                    <div className=''></div>
+                    <div className='mt-0 text-xl m-auto text-white  bg-dark_mode rounded-md ms-0 bg-opacity-75'> Merkez Servis Türkiyenin en geniş servis ağına sahipdir. Lg  Beyaz Eşyalarınız İçin İhtiyacınız olan servis hizmetini Merkez Servisten alın, size en yakın servisi 30 dk içinde yönlendirelim. </div>
+                    <div className=''></div>
+                    <div className='mx-auto ms-0 my-0'>
+                        <a href="tel:08503056090" className='text-white bg-indigo-600 font-medium rounded-lg text-md px-5 py-4 text-center hover:bg-ferrari_red hover:drop-shadow-md transition duration-300 ease-in-out m-2'>SERVİS ÇAĞIR</a>
+                        <a href="tel:08503056090" className='text-white bg-gray-500 font-medium rounded-lg text-md px-5 py-4 text-center hover:bg-ferrari_red hover:drop-shadow-md transition duration-300 ease-in-out m-2'>0850 850 8550</a>
+                    </div>
+
                 </div>
+
             </div>
 
 
