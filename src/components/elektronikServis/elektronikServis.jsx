5a531211d35a2c2b8776d372f0c0abe836d2fd2e
ismsahin <ismsahin6182@hotmail.com>
2023-11-12 22:01:32 +0300

    yazılara ekleme

--- src/components/elektronikServis/elektronikServis.jsx
+++ src/components/elektronikServis/elektronikServis.jsx
@@ -2,7 +2,63 @@ import React from 'react'
 
 const elektronikServis = () => {
   return (
-    <div>elektronikServis</div>
+    <div className=' min-h-screen gap-4 justify-center m-0'>
+
+      <div className='m-0 object-cover bg-cover bg-center xl:container bg-[url("https://images.pexels.com/photos/19014645/pexels-photo-19014645/free-photo-of-gokyuzu-desen-doku-duvar.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")] bg-center'>
+
+
+        <div className="  min-h-450 gap-4 flex-wrap flex justify-center items-center p-6">
+          <div>
+            <img className="max-h-96 object-cover rounded-3xl" src="https://images.pexels.com/photos/719399/pexels-photo-719399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+          </div>
+
+          <div className='bg-gray-400 rounded-3xl'>
+            <p className='text-white flex items-center p-1 text-center h-96 max-w-lg mt-1'>Elektronik cihazlarınızın tamiratı için güvenilir ve profesyonel bir işyeri arıyorsanız, doğru yerdesiniz! İşletmemiz, deneyimli teknisyen kadrosuyla beraber her türlü elektronik cihazınızın sorunlarını onarırken, size kusursuz bir hizmet sunmaktadır.
+              Müşteri memnuniyeti bizim için en önemli önceliktir. Her marka ve modele uygun tamir hizmetleri sunmaktayız. Televizyonlar, bilgisayarlar, oyun konsolları, tabletler, ses sistemleri ve daha birçok elektronik cihaz için tamiratlar gerçekleştiriyoruz. Ekran değişimi, anakart onarımı, güç kaynağı değişimi ve yazılım sorunları gibi farklı hizmetlerimiz bulunmaktadır.
+              Uzman teknisyenlerimiz, son teknoloji ekipmanlarla donatılmış atölyemizde çalışmalarını sürdürmektedir. Tamir işlemleri sırasında özenle orijinal yedek parçalar kullanıyoruz ve cihazınızın fabrika ayarlarına geri dönmesini sağlıyoruz. Tamir süreci boyunca size düzenli bilgi aktararak, her adımda ne olduğunu anlamanızı ve memnuniyetinizi sağlamayı hedefliyoruz. Fiyatlandırma konusunda da rekabetçi bir politika izliyoruz. Hizmetlerimizi makul fiyatlarla sunarak, müşterilerimize bütçe dostu bir hizmet sunuyoruz. Tamirat sonrasında cihazınızın tekrar performansını ve işlevselliğini geri kazanmasını sağlayarak, size uzun vadeli bir çözüm sunuyoruz.
+            </p>
+          </div>
+        </div>
+
+
+        <div className=" w-full gap-4 flex-wrap flex justify-center items-center p-6">
+          <div className="w-96 min-h-390 p-2 bg-white rounded-xl transition-all hover:-translate-y-2 duration-300 shadow-lg shadow-black hover:shadow-2xl">
+
+            <img className="h-64 object-cover w-96 rounded-xl" src="https://images.pexels.com/photos/2582937/pexels-photo-2582937.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+            <div className="p-2">
+
+              <h2 className="font-bold text-lg mb-2 ">Beyaz Eşya Servisi</h2>
+
+              <p className="text-sm text-gray-600">İhtiyacınız olan servis hizmetini Merkez Servisten alın, size en yakın servisi 30 dk içinde yönlendirelim.</p>
+            </div>
+          </div>
+
+          <div className="w-96 min-h-390 p-2 bg-white rounded-xl transition-all hover:-translate-y-2 duration-300 shadow-lg shadow-black hover:shadow-2xl">
+
+            <img className="h-64 object-cover w-96 rounded-xl" src="https://images.pexels.com/photos/13625784/pexels-photo-13625784.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+            <div className="p-2">
+
+              <h2 className="font-bold text-lg mb-2 ">Kombi Servisi</h2>
+
+              <p className="text-sm text-gray-600">İhtiyacınız olan servis hizmetini Merkez Servisten alın, size en yakın servisi 30 dk içinde yönlendirelim.</p>
+            </div>
+          </div>
+
+          <div className="w-96 min-h-390 p-2 bg-white rounded-xl transition-all hover:-translate-y-2 duration-300 shadow-lg shadow-black hover:shadow-2xl">
+
+            <img className="h-64 object-cover w-96 rounded-xl" src="https://media.istockphoto.com/id/1257647898/tr/foto%C4%9Fraf/iki-profesyonel-teknisyen-%C3%BCniformal%C4%B1-i%C5%9F%C3%A7iler-kapal%C4%B1-mekanlarda-duvara-televizyon-d%C3%B6%C5%9Fer.jpg?b=1&s=612x612&w=0&k=20&c=t0hExPx5ReB3e2Kt3InHeHzK3frMac1HkVprNg3SHsU=" alt="" />
+            <div className="p-2">
+
+              <h2 className="font-bold text-lg mb-2 ">Klima Servisi</h2>
+
+              <p className="text-sm text-gray-600">İhtiyacınız olan servis hizmetini Merkez Servisten alın, size en yakın servisi 30 dk içinde yönlendirelim.</p>
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/teknikServis/teknikServis.jsx
+++ src/components/teknikServis/teknikServis.jsx
@@ -2,16 +2,69 @@ import React from 'react'
 
 const teknikServis = () => {
   return (
-    <div className='w-full  min-h-screen gap-4 justify-center items-center grid grid-cols-2'>
-      <div className='max-h-96 '>
-      <img className=" object-cover  rounded-xl" src="https://images.pexels.com/photos/5463575/pexels-photo-5463575.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+    <div className=' min-h-screen gap-4 justify-center m-0'>
+
+      <div className='m-0 object-cover bg-cover bg-center xl:container bg-[url("https://images.pexels.com/photos/19014645/pexels-photo-19014645/free-photo-of-gokyuzu-desen-doku-duvar.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")] bg-center'>
+
+
+        <div className="  min-h-450 gap-4 flex-wrap flex justify-center items-center p-6">
+          <div>
+            <img className="max-h-96 object-cover rounded-3xl" src="https://images.pexels.com/photos/5463575/pexels-photo-5463575.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+          </div>
+
+          <div className='bg-gray-400 rounded-3xl'>
+            <p className='text-white flex items-center p-1 text-center h-96 max-w-lg mt-1'>İş yerimiz, profesyonel ekibimizle birlikte sorunlarınızı çözmek için burada.
+              Buzdolabınızın soğutma sorunu mu var? Klimanızın verimliliği düştü mü? Bizimle iletişime geçin, size en iyi tamir hizmetini sunalım.
+              Ev eşyalarınızda oluşan arızalar sizi üzmesin. Tamir işleriniz için doğru adres burası!
+              Buzdolabı, klima, çamaşır makinesi ve daha fazlası... Biz, ev eşyalarınızı tamir etmek için her zaman hazırız.
+              Eşyalarınızı tamir etmek için tecrübeli ve uzman ekibimize güvenin. Memnuniyetiniz bizim önceliğimizdir.
+              Ev eşyalarınızın uzun ömürlü olması için bakım ve tamir işlerini bize bırakın. Size zaman ve maliyet tasarrufu sağlayacağız.
+              İş yerimizde, yenilikçi tamir teknikleri ve yüksek kaliteli yedek parçalarla cihazlarınızı orijinal durumuna getiriyoruz.
+            </p>
+          </div>
+        </div>
+
+
+        <div className=" w-full gap-4 flex-wrap flex justify-center items-center p-6">
+          <div className="w-96 min-h-390 p-2 bg-white rounded-xl transition-all hover:-translate-y-2 duration-300 shadow-lg shadow-black hover:shadow-2xl">
+
+            <img className="h-64 object-cover w-96 rounded-xl" src="https://www.servisfonitürkiye.com/assets/images/blog/blog-img1.jpg" alt="" />
+            <div className="p-2">
+
+              <h2 className="font-bold text-lg mb-2 ">Beyaz Eşya Servisi</h2>
+
+              <p className="text-sm text-gray-600">İhtiyacınız olan servis hizmetini Merkez Servisten alın, size en yakın servisi 30 dk içinde yönlendirelim.</p>
+            </div>
+          </div>
+
+          <div className="w-96 min-h-390 p-2 bg-white rounded-xl transition-all hover:-translate-y-2 duration-300 shadow-lg shadow-black hover:shadow-2xl">
+
+            <img className="h-64 object-cover w-96 rounded-xl" src="https://www.servisfonitürkiye.com/assets/images/blog/blog-img2.jpg" alt="" />
+            <div className="p-2">
+
+              <h2 className="font-bold text-lg mb-2 ">Kombi Servisi</h2>
+
+              <p className="text-sm text-gray-600">İhtiyacınız olan servis hizmetini Merkez Servisten alın, size en yakın servisi 30 dk içinde yönlendirelim.</p>
+            </div>
+          </div>
+
+          <div className="w-96 min-h-390 p-2 bg-white rounded-xl transition-all hover:-translate-y-2 duration-300 shadow-lg shadow-black hover:shadow-2xl">
+
+            <img className="h-64 object-cover w-96 rounded-xl" src="https://www.servisfonitürkiye.com/assets/images/blog/blog-img3.jpg" alt="" />
+            <div className="p-2">
+
+              <h2 className="font-bold text-lg mb-2 ">Klima Servisi</h2>
+
+              <p className="text-sm text-gray-600">İhtiyacınız olan servis hizmetini Merkez Servisten alın, size en yakın servisi 30 dk içinde yönlendirelim.</p>
+            </div>
+          </div>
+          
+
+        </div>
       </div>
-
-      <div>
-
-      </div>
-
     </div>
+
+
   )
 }
 
